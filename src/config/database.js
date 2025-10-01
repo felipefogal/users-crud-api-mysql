21c9878a80c2c008b64c773fcb1681c19d7dbd58
Felipe Garcia Fogal <felipefogal@gmail.com>
2025-10-01 10:47:47 -0300

    Criando arquivo .env e criando também conexão a banco Mysql na AWS

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,35 @@
-import { Sequelize } from 'sequelize';
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv/config.js";
 
-const sequelize = new Sequelize('users_db', 'root', '', {
-  host: 'localhost',
-  dialect: 'mysql'
+const dbName = process.env.DB_NAME;
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASSWORD;
+const dbHost = process.env.DB_HOST;
+
+const sequelize = new Sequelize(dbName, dbUser, dbPassword, {
+  host: dbHost,
+  port: 3306,
+  dialect: "mysql",
+  pool: {
+    min: 1,
+    max: 5,
+    idle: 10000, // Desconexão após 10s de inatividade
+  },
 });
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully.");
+  })
+  .catch((err) => {
+    console.log("Unable to connect to the database:", err);
+  });
+
+/*Conexão local*/
+// const sequelize = new Sequelize(process.env.DB_LOCAL_NAME, process.env.DB_LOCAL_USER, process.env.DB_LOCAL_PASSWORD, {
+//   host: 'localhost',
+//   dialect: 'mysql'
+// });
 
 export default sequelize;
