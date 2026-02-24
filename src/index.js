23137818b746763678c393bb2e8e746ec8c85916
Felipe Garcia Fogal <felipefogal@gmail.com>
2026-02-24 19:25:52 -0300

    Modificação do index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import productRoutes from "./routes/productRoute.js";
 import sequelize from "./config/database.js";
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use("/api", userRoutes);
