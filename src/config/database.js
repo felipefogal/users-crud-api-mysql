e7eb5d0aff3a36f9c6ea5bf06cbaa65d859b0e4d
Felipe Garcia Fogal <felipefogal@gmail.com>
2026-03-30 18:29:42 -0300

    Inicio do trabalho de criação de APIs

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 import dotenv from "dotenv/config.js";
 
-// /* Conexão AWS */
+/* Conexão AWS */
 // const dbName = process.env.DB_NAME;
 // const dbUser = process.env.DB_USER;
 // const dbPassword = process.env.DB_PASSWORD;
@@ -17,7 +17,7 @@ import dotenv from "dotenv/config.js";
 //     idle: 10000, // Desconexão após 10s de inatividade
 //   },
 // });
-// /* Conexão AWS */
+/* Conexão AWS */
 
 /*Conexão local*/
 const sequelize = new Sequelize(process.env.DB_LOCAL_NAME, process.env.DB_LOCAL_USER, process.env.DB_LOCAL_PASSWORD, {
